637f73bac59fa4111c039356f4df0a0183e19756
Lucy.H <june9014@gmail.com>
2019-09-07 18:52:04 +1000

    insert sql not working

--- src/FirstJDBCExample.java
+++ src/FirstJDBCExample.java
@@ -1,60 +1,80 @@
 import java.sql.*;
 
 public class FirstJDBCExample {
+
     // JDBC driver name and database URL
     static final String DB_URL = "jdbc:mysql://localhost:3306/emp?useLegacyDatetimeCode=false&serverTimezone=UTC";
 
     //  Database credentials
     static final String USER = "root";
-    static final String PASS = "fkfmzmfjqm84";
+    static final String PASS = "mysql123";
 
     // Method to create a new DB connection and display data from table
     public static void getDBConn(){
         Connection conn = null;
         Statement stmt = null;
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
+
         try{
-            //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
-
             //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
+            //System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            System.out.println("Connected to database..."+conn.toString());
-
+            System.out.println("Connected to database..." + conn.toString());
 
             //STEP 4: Execute a query
             System.out.println ("Creating statement...");
             stmt = conn.createStatement();
+
+            //insert new rows
+            pstmt = conn.prepareStatement("Insert into Employees(id, age, FIRST, LAST) values (?,?,?,?)");
+
+            //place the values into the position
+            pstmt.setInt(1, 110);
+            pstmt.setInt(2, 18);
+            pstmt.setString(3, "Brown");
+            pstmt.setString(4, "S"); // this insert command not working, no error detected
+
+            //retrieve all employees
             String sql;
-            sql = "SELECT id FROM Employees";
-            ResultSet rs = stmt.executeQuery(sql);
-
-            //STEP 5: Extract data from result set
-            while(rs.next()){
-                //Retrieve by column name
-                int id  = rs.getInt("id");
-
-                //Display values
-                System.out.print("ID: " + id);
-            }
-            //STEP 6: Clean-up environment
-            rs.close();
-            stmt.close();
-            conn.close();
-
-
-        }catch(SQLException se){
+            sql = "SELECT * FROM Employees";
+            rs = stmt.executeQuery(sql);
+        }
+        catch(SQLException se){
             //Handle errors for JDBC
             se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
+        }
+        finally {
+            if(rs != null){
+                try{
+                    //STEP 2: Register JDBC driver
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+
+                    //STEP 5: Extract data from result set
+                    while(rs.next()){
+                        //Retrieve by column name
+                        int id  = rs.getInt("id");
+                        int age = rs.getInt("age");
+                        String first = rs.getString("first");
+                        String last = rs.getString("last");
+
+                        //Display values
+                        System.out.println("ID: " + id + "\tAge: "+ age + "\t" + first + "."+ last);
+                    }
+
+                    //STEP 6: Clean-up environment
+                    rs.close();
+                    stmt.close();
+                    conn.close();
+                }
+                catch(Exception e){
+                    //Handle errors for Class.forName
+                    e.printStackTrace();
+                }
+            }
         }
 
-
     }//end try
-
-
 
     //main
     public static void main(String[] args) {
